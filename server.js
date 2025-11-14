404e9d7f0595b521f06b2d9fe75156307a887e26
Joelnin <joelninr@gmail.com>
2025-11-14 10:15:30 -0400

    Installed swagger

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, x-Requested-width, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require('./routes'));
 
 
